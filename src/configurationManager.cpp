24533282c3189719b3f1303e71fc7a2bad3cde05
Ryan Evan Wolf <ryanevwolf@gmail.com>
2018-01-01 21:29:24 +0200

    added camera info topics and populated it for the rectified camera settings (post rectification)

--- src/configurationManager.cpp
+++ src/configurationManager.cpp
@@ -14,20 +14,54 @@ configurationManager::configurationManager(std::string stereoInputFile)
 	getQServ=n.advertiseService("bumblebee_configuration/getQ",&configurationManager::Q,this);
 	infoLPub=n.advertise<sensor_msgs::CameraInfo>("bumblebee_configuration/left/info",20,true);
 	infoRPub=n.advertise<sensor_msgs::CameraInfo>("bumblebee_configuration/right/info",20,true);
-	//still to populate camera info properly TODO
+	
+	//assuming the cameraInfo is ideal rectified data
+	//i.e. no rotation information really required, everything set to identity
 	sensor_msgs::CameraInfo configMessageL, configMessageR;
+	configMessageL.header.frame_id="RectifiedLeftFrame";
+	configMessageR.header.frame_id="RectifiedRightFrame";
+	
 	configMessageL.height=bumblebee.L_iMapx_.rows;
 	configMessageL.width=bumblebee.L_iMapx_.cols;
+	configMessageR.height=bumblebee.L_iMapx_.rows;
+	configMessageR.width=bumblebee.L_iMapx_.cols;
 	
+	configMessageL.distortion_model="plumb_bob";
+	configMessageR.distortion_model="plumb_bob";
+	for(int index=0;index<5;index++)
+	{
+		configMessageL.D.push_back(0);
+		configMessageR.D.push_back(0);
+	}
+	for(int row=0;row<3;row++)
+	{
+		configMessageL.R[3*row+row]=1;//set identity rotation
+		configMessageR.R[3*row+row]=1;
+		for(int col=0;col<3;col++)
+		{
+			configMessageL.K[3*row+col]=bumblebee.P_l_.at<double>(row,col);
+			configMessageR.K[3*row+col]=bumblebee.P_r_.at<double>(row,col);
+		}
+	}
 	
-	/*configMessageL.distortion_model="plumb_bob";
-	configMessageL.D[0]=0;
-	configMessageL.D[1]=0;
-	configMessageL.D[2]=0;
-	configMessageL.D[3]=0;
-	configMessageL.D[4]=0;*/
-
+	for(int index=0;index<3;index++)
+	{
+		for(int col=0;col<4;col++)
+		{
+			configMessageL.P[4*index+col]=bumblebee.P_l_.at<double>(index,col);
+			configMessageR.P[4*index+col]=bumblebee.P_r_.at<double>(index,col);
+		}
+	}
 	
+	configMessageL.roi.x_offset=bumblebee.l_ROI_.x;
+	configMessageL.roi.y_offset=bumblebee.l_ROI_.y;
+	configMessageL.roi.height=bumblebee.l_ROI_.height;
+	configMessageL.roi.width=bumblebee.l_ROI_.width;
+	
+	configMessageR.roi.x_offset=bumblebee.r_ROI_.x;
+	configMessageR.roi.y_offset=bumblebee.r_ROI_.y;
+	configMessageR.roi.height=bumblebee.r_ROI_.height;
+	configMessageR.roi.width=bumblebee.r_ROI_.width;
 	
 	infoLPub.publish(configMessageL);
 	infoRPub.publish(configMessageR);
